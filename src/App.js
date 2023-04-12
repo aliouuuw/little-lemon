4d25db185ed2c113cfdd17b1f10c67f6976513cd
PCAW <wadealiou00@gmail.com>
2023-04-12 06:45:10 -0400

    Initialization

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
 import { ChakraProvider } from "@chakra-ui/react";
-import Header from "./components/Header";
+/* import Header from "./components/Header";
 import LandingSection from "./components/LandingSection";
 import ProjectsSection from "./components/ProjectsSection";
 import ContactMeSection from "./components/ContactMeSection";
 import Footer from "./components/Footer";
 import Alert from "./components/Alert";
 
-import { AlertProvider } from "./context/alertContext";
+import { AlertProvider } from "./context/alertContext"; */
 
 
 function App() {
   return (
-    <>
-    <h1>Hello World</h1>
-    </>
+    <ChakraProvider>
+      <h1>Hello Little Lemon</h1>
+    </ChakraProvider>
   );
 }
 
